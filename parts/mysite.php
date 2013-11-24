c221f9f3661b42cd936151d7d5c0d9f2c56180f3
Ivan <ivan@ivan-SATELLITE-C660.(none)>
2013-11-24 20:06:33 +0000

    laga db merge

--- parts/mysite.php
+++ parts/mysite.php
@@ -1,11 +1,20 @@
 <h1>Mínar Síður</h1>
 <p> Þú ert skráður inn sem <?php echo $_COOKIE['user']; ?> </p>
-<a href="?part=logout"> Útskrá</a>
 
-<h2>Mín veðmál</h2>
-<div class="bets">
 	<?php
-		$db = new PDO('sqlite:football.db');
+	$db = new PDO('sqlite:football.db');
+
+	$results = $db->query("SELECT * FROM USERS WHERE Username='".$_COOKIE['user']."'");
+
+	foreach($results as $data)
+		{
+			echo "Innistæða þín er: ".$data['Cash']."<br>";
+		}
+
+	echo "<a href='?part=logout'> Útskrá</a>";
+
+	echo "<h2>Mín veðmál</h2>
+	<div class='bets'>";
 
 		$results = $db->query("SELECT * FROM BETS WHERE Username='".$_COOKIE['user']."'");
 		$found = 0;
@@ -19,12 +28,13 @@
 				if($found==0){echo "<ul>";}
 				$found=1;
 
-				if($data['Bet']=='home'){echo "<li>Þú veðjaðir á".$game['Home']." í leik: ".$game['Home']."(home) Vs. ".$game['Away']."(away). Stuðull".$game['Homewin']."</li>";}
-				else if($data['Bet']=='away'){echo "<li>Þú veðjaðir á".$game['Away']." í leik: ".$game['Home']."(home) Vs. ".$game['Away']."(away). Stuðull".$game['Awaywin']."</li>";}
-				else {echo "<li>Þú veðjaðir á jafntefli í leik: ".$game['Home']."(home) Vs. ".$game['Away']."(away). Stuðull".$game['Draw']."</li>";}
+				if($data['Bet']=='home'){echo "<li>Þú veðjaðir ".$data['Amount']." á ".$game['Home']." í leik: ".$game['Home']."(home) Vs. ".$game['Away']."(away). Stuðull: ".($game['Homewin']/100)."</li>";}
+				else if($data['Bet']=='away'){echo "<li>Þú veðjaðir ".$data['Amount']." á ".$game['Away']." í leik: ".$game['Home']."(home) Vs. ".$game['Away']."(away). Stuðull: ".($game['Awaywin']/100)."</li>";}
+				else {echo "<li>Þú veðjaðir ".$data['Amount']." á jafntefli í leik: ".$game['Home']."(home) Vs. ".$game['Away']."(away). Stuðull".($game['Draw']/100)."</li>";}
 			}
 		} 
 		if($found==0){echo"Þú hefur ekki veðjað á neitt nýlega.";}
 		else {echo "</ul>";}
 	?>
 </div>
+<a href="?part=me&getwinnings=true">Sækja Vinninga</a>
